6174975910da09147e980f2b07d349bd8cde9b1a
jpfriedman18 <jpfriedman18@gmail.com>
2016-03-12 17:31:12 -0500

    Add User action button listeners/AJAX requests Note: still getting 500 error from backend

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,113 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+const myApp = {
+  BASE_URL: "http://localhost:3000"
+};
+
+//Account AJAX requests
+
+//Makes Sign In AJAX request
+//called by clicking sign-in button OR successfully signing up
+let signIn = function(e){
+  e.preventDefault();
+  var formData = new FormData(e.target);
+  $.ajax({
+    url: myApp.BASE_URL + '/sign-in',
+    method: 'POST',
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    console.log(data);
+    myApp.user = data.user;
+    console.log(myApp.user);
+    $('.signed-out').hide();
+    $('.signed-in').show();
+    $('#sign-in-modal').modal('hide');
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+let setSignUpListener = function(){
+  //Create new user
+  $('#sign-up').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.BASE_URL + '/sign-up',
+      method: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      console.log(data);
+      signIn(e);
+      $('#sign-up-modal').modal('hide');
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+};
+
+let setSignInListener = function(){
+  //Login as existing user
+  $('#sign-in').on('submit', function(e) {
+    e.preventDefault();
+    signIn(e);
+  });
+};
+
+let setChangePasswordListener = function(){
+  //Change password of currently logged-in user
+  $('#change-password').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.BASE_URL + '/change-password/' + myApp.user.id,
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      console.log(data);
+      $('#change-password-modal').modal('hide');
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+};
+
+let setSignOutListener = function(){
+  //Log out
+  $('#sign-out-button').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: myApp.BASE_URL + '/sign-out/' + myApp.user.id,
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+    }).done(function() {
+      console.log("Logged Out!");
+      $('.signed-out').show();
+      $('.signed-in').hide();
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+};
+
+$(document).ready(() => {
+  $('.signed-out').show();
+  $('.signed-in').hide();
+  setSignUpListener();
+  setSignInListener();
+  setChangePasswordListener();
+  setSignOutListener();
+});
